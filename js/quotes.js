3b6a64b5b68562f19da525b7c526afc6733b6192
jessica van rooyen <jessicav556@gmail.com>
2018-01-25 15:40:42 +0200

    complete files

---
+++ js/quotes.js
@@ -0,0 +1,54 @@
+//create an array of javascript objects named quotes
+const quotes = [
+	{
+		quote: 'Nobody exists on purpose. Nobody belongs anywhere. We\'re all going to die. Come watch TV.',
+		source: 'Morty',
+		citation: 'Rixty Minutes',
+		year: 2014,
+		tags: ['alternate', 'realities']
+	},
+	{
+		quote: 'Weddings are basically funerals with cake.',
+		source: 'Rick',
+		citation: 'The Weddings Squanchers',
+		tags: ['true', 'dat']
+	},
+	{
+		quote: 'Because I don\'t respect therapy; because I\'m a scientist; because I invent, transform, create, and destroy for a living, and when I don\'t like something about the world, I change it.',
+		source: 'Rick',
+		citation: 'Pickle Rick',
+		year: 2017,
+		tags: ['life', 'advice']
+	},
+	{
+		quote: 'What, so everyone\'s supposed to sleep every single night now. You realize that nighttime makes up half of all time?',
+		source: 'Rick',
+		citation: 'Auto Erotic Assimilation',
+		year: 2015,
+		tags: ['sleep', 'less', 'do', 'more']
+	},
+	{
+		quote: 'Listen Morty, I hate to break it to you, but what people calls \'love\' is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science.',
+		source: 'Rick',
+		year: 2014,
+		tags: ['science is love', 'science is life']
+	},
+	{
+		quote: 'I know that new situations can be intimidating. You lookin’ around and it’s all scary and different, but y’know... meeting them head-on, charging into ‘em like a bull — that’s how we grow as people.',
+		source: 'Rick',
+		citation: 'Pilot',
+		year: 2014,
+		tags: ['righto!']
+	},
+	{
+		quote: 'Uncertainty is inherently unsustainable. Eventually, everything either is or isn\'t.',
+		source: 'Rick',
+		tags: ['mind', 'blown']
+	},
+	{
+		quote: 'Having a family doesn’t mean that you stop being an individual. You know the best thing you can do for the people that depend on you? Be honest with them, even if it means setting them free.',
+		source: 'Mr. Meeseeks',
+		citation: 'Meeseeks and Destroy',
+		tags: ['yep']
+	},
+];
---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+// create a function named getRandomQuote which selects a random quote then return selected quote
+function getRandomQuote() {
+	randomQuote = Math.floor(Math.random() * quotes.length);
+	return quotes[randomQuote];
+}
+
+// quotes automatically change after 30 seconds
+window.setInterval(printQuote, 30000);
+
+//  applies random background color when the printQuote function passes
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 256 );
+    let green = Math.floor(Math.random() * 256 );
+    let blue = Math.floor(Math.random() * 256 );
+    let rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+    document.body.style.background = rgbColor;
+}
+
+/* create a function named printQuote which:
+/  calls the getRandomQuotes function and stores the selected quote object in a variable;
+/  constructs a string containing different properties of the quote object;
+/  print random quote to page
+/ change background color onClick
+*/
+function printQuote() {
+	let quote = getRandomQuote();
+	let htmlPrint = '<p class="quote">' + quote.quote + '</p>';
+        htmlPrint += '<p class="source">' + quote.source;
+        
+        // if/else statement to determine if 'citation' or 'year' or both are present in object
+        if ( quote.citation !== undefined && quote.year !== undefined) {
+            htmlPrint += '<span class="citation">' + quote.citation + '</span>';
+            htmlPrint += '<span class="year">' + quote.year + '</span>';
+        } else if (quote.citation !== undefined){
+            htmlPrint += '<span class="citation">' + quote.citation + '</span>';
+        } else if (quote.year !== undefined) {
+            htmlPrint += '<span class="year">' + quote.year + '</span>';
+        }
+	htmlPrint += '</p>';
+	htmlPrint += '<p class="tags">Tags: ' + quote.tags.join(', ') + '</p>';
+
+    document.getElementById('quote-box').innerHTML = htmlPrint;
+
+	return getRandomColor();
+}
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener('click', printQuote, false);
